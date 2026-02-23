21093e815fa581dbb2942a9b3e85007d0db6775f
Farhad Nuri <231006312@eastdelta.edu.bd>
2026-02-24 01:43:14 +0600

    Added the tab swithing functionality

--- script/display.js
+++ script/display.js
@@ -12,9 +12,33 @@ function showAllJobs() {
     if (job.status === "all") {
       const jobCard = createJobCard(job);
       allContainer.appendChild(jobCard);
-    } 
+    }
   }
-
+  showHideEmptyMessages();
 }
 
+function showHideEmptyMessages() {
+  const allContainer = document.getElementById("allJobsContainer");
+  const allEmpty = document.getElementById("allEmpty");
+  if (allContainer.children.length === 0) {
+    allEmpty.classList.remove("hidden");
+  } else {
+    allEmpty.classList.add("hidden");
+  }
 
+  const interviewContainer = document.getElementById("interviewJobsContainer");
+  const interviewEmpty = document.getElementById("interviewEmpty");
+  if (interviewContainer.children.length === 0) {
+    interviewEmpty.classList.remove("hidden");
+  } else {
+    interviewEmpty.classList.add("hidden");
+  }
+
+  const rejectedContainer = document.getElementById("rejectedJobsContainer");
+  const rejectedEmpty = document.getElementById("rejectedEmpty");
+  if (rejectedContainer.children.length === 0) {
+    rejectedEmpty.classList.remove("hidden");
+  } else {
+    rejectedEmpty.classList.add("hidden");
+  }
+}
---
+++ script/tabs.js
@@ -0,0 +1,55 @@
+function setupTabButtons() {
+    const allTabBtn = document.querySelector('[data-tab="all"]');
+    const interviewTabBtn = document.querySelector('[data-tab="interview"]');
+    const rejectedTabBtn = document.querySelector('[data-tab="rejected"]');
+
+    allTabBtn.addEventListener("click", function () {
+        switchToTab("all");
+    });
+
+    interviewTabBtn.addEventListener("click", function () {
+        switchToTab("interview");
+    });
+
+    rejectedTabBtn.addEventListener("click", function () {
+        switchToTab("rejected");
+    });
+}
+
+
+function switchToTab(tabName) {
+
+    const allTabBtn = document.querySelector('[data-tab="all"]');
+    const interviewTabBtn = document.querySelector('[data-tab="interview"]');
+    const rejectedTabBtn = document.querySelector('[data-tab="rejected"]');
+
+    allTabBtn.classList.remove("active", "bg-blue-500", "text-white");
+    interviewTabBtn.classList.remove("active", "bg-blue-500", "text-white");
+    rejectedTabBtn.classList.remove("active", "bg-blue-500", "text-white");
+
+    if (tabName === "all") {
+        allTabBtn.classList.add("active", "bg-blue-500", "text-white");
+    } else if (tabName === "interview") {
+        interviewTabBtn.classList.add("active", "bg-blue-500", "text-white");
+    } else if (tabName === "rejected") {
+        rejectedTabBtn.classList.add("active", "bg-blue-500", "text-white");
+    }
+
+    const allTab = document.getElementById("allTab");
+    const interviewTab = document.getElementById("interviewTab");
+    const rejectedTab = document.getElementById("rejectedTab");
+
+    allTab.classList.add("hidden");
+    interviewTab.classList.add("hidden");
+    rejectedTab.classList.add("hidden");
+
+    if (tabName === "all") {
+        allTab.classList.remove("hidden");
+    } else if (tabName === "interview") {
+        interviewTab.classList.remove("hidden");
+    } else if (tabName === "rejected") {
+        rejectedTab.classList.remove("hidden");
+    }
+
+    
+}
